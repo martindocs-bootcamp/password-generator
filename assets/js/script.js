2567e88e06faa12ec73af3599ca1b94b317c3c79
Marcin Tatarski <martin.tatarski>
2023-11-13 23:57:52 +0000

    Create function to get password options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+const specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+const lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+const upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -90,7 +90,47 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let stop = true;
+  let output = [];
+  let passwordLength = 0;
+  
+  while (stop) {
+    passwordLength = prompt('Enter password length between [8 - 128] characters: ');
+        
+    if(passwordLength === null || Number(passwordLength) >= 8 && Number(passwordLength) <= 128){
+      stop = false
+    }else if(passwordLength !== null && (Number(passwordLength) < 8 || Number(passwordLength) > 128)) {
+      alert('Please enter a valid password length');
+    }else{
+      alert('Only numbers between [8 - 128] are allowed');
+    }
+  }
 
+  if(passwordLength !== null){
+    const lowerCase = confirm('Do you want to include lowercase characters');
+    if(lowerCase) {
+      output = [...output, ...lowerCasedCharacters];
+    }
+  
+    const upperCase = confirm('Do you want to include uppercase characters');
+    if(upperCase) {
+      output = [...output, ...upperCasedCharacters];
+    }
+    
+    const numericValues = confirm('Do you want to include numeric values');
+    if(numericValues) {
+      output = [...output, ...numericCharacters];
+    }
+
+    const specialChars = confirm('Do you want to include special characters [$@%&*, etc]');  
+    if(specialChars) {
+      output = [...output, ...specialCharacters];
+    }
+
+    return {output, passwordLength}
+  }
+
+  return false;
 }
 
 // Function for getting a random element from an array
@@ -100,7 +140,11 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+  const getPass = getPasswordOptions();
 
+  if(getPass){
+    console.log(getPass);
+  }
 }
 
 // Get references to the #generate element
