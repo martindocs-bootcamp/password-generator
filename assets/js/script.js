d20a965224632666de93db1f5106f9a1f16858d2
Martin Tatarski <martin.tatarski@gmail.com>
2023-11-14 11:42:12 +0000

    Create 'getRandom' function for generating randomized password

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,14 +91,14 @@ const upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
   let stop = true;
-  let output = [];
+  let charTypes = [];
   let passwordLength = 0;
   
   while (stop) {
     passwordLength = prompt('Enter password length between [8 - 128] characters: ');
         
     if(passwordLength === null || Number(passwordLength) >= 8 && Number(passwordLength) <= 128){
-      stop = false
+      stop = false;
     }else if(passwordLength !== null && (Number(passwordLength) < 8 || Number(passwordLength) > 128)) {
       alert('Please enter a valid password length');
     }else{
@@ -109,39 +109,58 @@ function getPasswordOptions() {
   if(passwordLength !== null){
     const lowerCase = confirm('Do you want to include lowercase characters');
     if(lowerCase) {
-      output = [...output, ...lowerCasedCharacters];
+      charTypes = [...charTypes, ...lowerCasedCharacters];
     }
   
     const upperCase = confirm('Do you want to include uppercase characters');
     if(upperCase) {
-      output = [...output, ...upperCasedCharacters];
+      charTypes = [...charTypes, ...upperCasedCharacters];
     }
     
     const numericValues = confirm('Do you want to include numeric values');
     if(numericValues) {
-      output = [...output, ...numericCharacters];
+      charTypes = [...charTypes, ...numericCharacters];
     }
 
     const specialChars = confirm('Do you want to include special characters [$@%&*, etc]');  
     if(specialChars) {
-      output = [...output, ...specialCharacters];
+      charTypes = [...charTypes, ...specialCharacters];
     }
 
-    return {output, passwordLength}
+    
+    return {
+      passwordLength: passwordLength, 
+      charTypes: charTypes.join('')
+    }
   }
 
   return false;
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom(length, chars) {
+  // generated random password 
+  let randomPassword = '';
 
+  // unsigned 32-bit array used to get random numbers, where length is the user password length 
+  const array = new Uint32Array(length);
+
+  // generte randoms numbers in the array
+  self.crypto.getRandomValues(array);
+
+  // loop through array and add new random character to the 'randomPassword' variable
+  for(const randomNum of array){  
+    // the % remainder ensures that the random characters won't be out of the range of 'chars' length variable
+    randomPassword += chars[randomNum % chars.length]; 
+  }
+  
+  return randomPassword;
 }
 
 // Function to generate password with user input
 function generatePassword() {
   const getPass = getPasswordOptions();
-
+  
   if(getPass){
     console.log(getPass);
   }
