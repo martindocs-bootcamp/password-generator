07fc9da4fb620984720c44343881381680fda8f1
Marcin Tatarski <martin.tatarski>
2023-11-17 00:08:45 +0000

    Add comments to javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,23 +90,29 @@ const upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let stop = true;
-  let charTypes = [];
-  let passwordLength = 0;
+  let stop = true; // Variable to control the loop
+  let charTypes = []; // Array to store selected character types
+  let passwordLength = 0; // Variable to store the chosen password length
   
+  // Loop to ensure a valid password length is entered
   while (stop) {
+    // Prompt the user for password length
     passwordLength = prompt('Enter password length between [8 - 128] characters: ');
         
     if(passwordLength === null || Number(passwordLength) >= 8 && Number(passwordLength) <= 128){
-      stop = false;
+      stop = false; // Exit the loop if a valid input is received
     }else if(passwordLength !== null && (Number(passwordLength) < 8 || Number(passwordLength) > 128)) {
+      // Display an alert for an invalid password length
       alert('Please enter a valid password length');
     }else{
+      // Display an alert for non-numeric input
       alert('Only numbers between [8 - 128] are allowed');
     }
   }
 
+  // Check if the user provided a password length (didn't click cancel)
   if(passwordLength !== null){
+    // Prompt user for character type options
     const lowerCase = confirm('Do you want to include lowercase characters');
     if(lowerCase) {
       charTypes = [...charTypes, ...lowerCasedCharacters];
@@ -127,28 +133,32 @@ function getPasswordOptions() {
       charTypes = [...charTypes, ...specialCharacters];
     }
 
+    // Check if at least one character type is chosen
     if(charTypes.length !== 0){
+      // Return an object with password options
       return {
         passwordLength: passwordLength, 
-        charTypes: charTypes.join('')
+        charTypes: charTypes.join('') // Convert the array to a string
       }
     }else{
+      // Display an alert if no character type is chosen
       alert('Ensure that at least one character type is chosen.')
     }
   }
 
+  // Return false if the user clicked cancel or didn't provide valid options
   return false;
 }
 
 // Function for getting a random element from an array
 function getRandom(length, chars) {
-  // generated random password 
+  // variable for storing generated random password 
   let randomPassword = '';
 
   // unsigned 32-bit array used to get random numbers, where length is the user password length 
   const array = new Uint32Array(length);
 
-  // generte randoms numbers in the array
+  // generte randoms numbers using  Web Cryptography API used to fill an array with cryptographically secure random values.
   self.crypto.getRandomValues(array);
 
   // loop through array and add new random character to the 'randomPassword' variable
@@ -157,6 +167,7 @@ function getRandom(length, chars) {
     randomPassword += chars[randomNum % chars.length]; 
   }
   
+  // return random generated password
   return randomPassword;
 }
 
@@ -184,32 +195,39 @@ function writePassword() {
   if(password) passwordText.value = password;
 }
 
-
-// copy to clipboard
+// Copy to clipboard
 const clipboard = document.querySelector('#clipboard-container');
 
+// Function to handle copying the password to the clipboard
 function copyToClipboard(e) {
+   // Select the password input element from the DOM
   const password = document.querySelector('#password');
+  // Check if the click event was triggered by the clipboard copy button and if the password is not empty
   if(e.target.className === 'clipboard-copy' && password.value !== ''){
+    // Select the clipboard copy button and the clipboard mark elements
     const copy = document.querySelector('.clipboard-copy');
     const mark = document.querySelector('.clipboard-mark');
 
+    // Apply fadeInOut animation to the copy button and the clipboard mark
     copy.style.animation = 'fadeInOut 0.5s ease-in-out';
     mark.style.animation = 'fadeInOut 0.5s ease-in-out';
     
+    // Hide the copy button and show the clipboard mark
     copy.classList.add('hide');
     mark.classList.remove('hide');
 
+    // Copy the password to the clipboard
     navigator.clipboard.writeText(password.value);
 
-     setTimeout(() => {
+    // Set a timeout to revert the UI changes
+    setTimeout(() => {
       mark.classList.add('hide'); 
       copy.classList.remove('hide');
-    }, 1000);
+    }, 1000); // Revert changes after 1s
   }  
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-clipboard.addEventListener('click', copyToClipboard)
+// Add event listener to copy password to clipboard
+clipboard.addEventListener('click', copyToClipboard);
